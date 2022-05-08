8375266b6adf18f5582d9975d3eb6b61c4c184b8
Shibin K B <58941292+ShibinKB7@users.noreply.github.com>
2022-05-08 11:48:53 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 class Simple{
     public static void main (String args[]){
         System.out.println("Hello, this is my first jave program to test automatic build Jenkins");
+        System.out.println("To test autmated build");
     }
     }
